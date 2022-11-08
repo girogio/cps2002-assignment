cb99a7078da4789a8f281dbe03f65f5e41387a96
girogio <giorgiogrigolo03@gmail.com>
2022-11-08 18:31:08 +0100

    Reverted the migration to modules

--- user-handler/run.js
+++ user-handler/run.js
@@ -1,37 +1,20 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
 
-const User = require('./src/models/User')
-
-require('dotenv').config()
-
+express = require('express')
+mongoose = require('mongoose')
+cors = require('cors')
+dotenv = require('dotenv')
 
 const app = express()
+const db = mongoose.connection
+
+dotenv.config()
 app.use(express.json())
 app.use(cors())
 
-//  Initialise database
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to database.'))
 
+app.use('/api/users', require('./src/routes/userRoute.js'))
 
-app.post('/users', (req, res) => {
-  const user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  })
-  user.save().then(() => {
-    res.send('User created.')
-  }).catch((error) => {
-    res.status(400).send(error)
-  })
-
-})
-
-
-// Start server
 app.listen(process.env.PORT, () => console.log(`Server started on port ${process.env.PORT}.`))
