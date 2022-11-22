f458a3fd1ab6b3fd83b9cbde19dc4da171e941f0
Girogio <giorgiogrigolo03@gmail.com>
2022-11-23 00:03:30 +0100

    Added the healthCheck route to the main file

--- user-handler-service/run.js
+++ user-handler-service/run.js
@@ -17,5 +17,7 @@ mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTo
 db.on('error', (error) => console.error(error))
 
 app.use('/api/users', require('./src/routes/userRoute.js'))
+app.use('/api/', require('./src/routes/healthRoute.js'))
+
 app.listen(process.env.SERVICE_PORT, () => console.log('Server started on port ' + process.env.SERVICE_PORT))
 db.once('open', () => console.log('Connected to database.' ))
