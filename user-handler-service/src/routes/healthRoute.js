6cdc905f0acb27ee19051839d77494ea71950adf
Girogio <giorgiogrigolo03@gmail.com>
2022-11-23 00:03:07 +0100

    Added a healthcheck route

---
+++ user-handler-service/src/routes/healthRoute.js
@@ -0,0 +1,8 @@
+express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.status(200).json({ success: true, message: 'Service is up and running.' })
+})
+
+module.exports = router
