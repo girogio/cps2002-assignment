cce90841c62989e71adfcafe9bcd0657286e6142
Girogio <giorgiogrigolo03@gmail.com>
2022-11-28 23:44:45 +0100

    Misc

--- gorju-frontend-service/next.config.js
+++ gorju-frontend-service/next.config.js
@@ -4,6 +4,11 @@ const path = require('path')
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
