a8fb453fc15f6cfe966f571f3e0f5292ee1299f6
Giorgio Grigolo <grongo@mbp.lan>
2022-12-06 00:41:25 +0100

    Added dialog for user editing and polished users page

--- gorju-frontend-service/src/pages/users.tsx
+++ gorju-frontend-service/src/pages/users.tsx
@@ -1,6 +1,6 @@
-
 import {
-  faEllipsisVertical
+  faEllipsisVertical,
+  faSync
 } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { AdminLayout } from '@layout'
@@ -8,7 +8,7 @@ import axios from 'axios'
 import type { NextPage } from 'next'
 import React, { useEffect } from 'react'
 import {
-  Button, Card, Dropdown, Form, Toast, ToastContainer, Alert
+  Button, Card, Dropdown, Form, Toast, ToastContainer, Alert, Modal, ModalBody
 } from 'react-bootstrap'
 
 type User = { _id: string, name: string, email: string, cars_rented: number, date_created: string, last_login: string }
@@ -16,8 +16,10 @@ type User = { _id: string, name: string, email: string, cars_rented: number, dat
 const UsersPage: NextPage = () => {
   const [users, setUsers] = React.useState<User[]>([])
   const [userServiceActive, setUserServiceActive] = React.useState<boolean>(false)
+  const [modalVisibile, setModalVisible] = React.useState<editModal>({ show: false, user_id: '' })
 
   type Toast = { title: string, body: string, type: string, visibility: boolean }
+  type editModal = { show: boolean, user_id: string }
 
   const [toasts, setToasts] = React.useState<Toast[]>([])
 
@@ -31,6 +33,16 @@ const UsersPage: NextPage = () => {
     setToasts(newToasts)
   }
 
+  const updateUsers = () => {
+    axios.get('http://localhost:3001/api/users')
+      .then((response) => {
+        setUsers(response.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
   // Prepare health check
   useEffect(() => {
     axios.get('http://localhost:3001/api/')
@@ -42,14 +54,8 @@ const UsersPage: NextPage = () => {
 
   // Subscribe to user fetch endpoint
   useEffect(() => {
-    axios.get('http://localhost:3001/api/users')
-      .then((response) => {
-        setUsers(response.data)
-      })
-      .catch((error) => {
-        console.log(error)
-      })
-  }, [users])
+    updateUsers()
+  }, [])
 
 
   // Utility function for deleting a user
@@ -83,12 +89,39 @@ const UsersPage: NextPage = () => {
           form.formBasicEmail.value = ''
           form.formBasicName.value = ''
           // Notify User
+          updateUsers()
           addToast('Success', response.data, 'info')
         }
       }).catch((error) => {
         addToast('Error', error.message, 'warning')
       })
   }
+
+  const editUser = (event: any) => {
+
+    // Prevent page refresh
+    event.preventDefault()
+    event.stopPropagation()
+
+    // Get form data
+    const form = event.currentTarget
+    const email = form.formBasicEmail.value
+    const name = form.formBasicName.value
+
+    // Send request
+    axios.patch('http://localhost:3001/api/users/' + modalVisibile.user_id, { name, email })
+      .then((response) => {
+        console.log(response)
+        if (response.status === 200) {
+          setModalVisible({ show: false, user_id: '' })
+          addToast('Success', response.data, 'info')
+          updateUsers()
+        }
+      }).catch((error) => {
+        addToast('Error', error.message, 'warning')
+      })
+  }
+
 
   return (
     <AdminLayout>
@@ -115,6 +148,38 @@ const UsersPage: NextPage = () => {
         </ToastContainer>
       </div>
       <div className="row">
+        {/* modal with form */}
+        <Modal
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+          show={modalVisibile.show}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+              Create User
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form onSubmit={editUser}>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control required type="email" placeholder="john@doe.com" />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicName">
+                <Form.Label>Full Name</Form.Label>
+                <Form.Control required type="name" placeholder="John Doe" />
+              </Form.Group>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={() => { setModalVisible({ show: false, user_id: '' }) }}>Close</Button>
+                <Button variant="primary" type="submit">Create</Button>
+              </Modal.Footer>
+            </Form>
+          </Modal.Body>
+        </Modal>
+        {/* end modal with form */}
+
         <div className="col-md-12">
 
           <Alert variant="danger" show={!userServiceActive}>
@@ -125,8 +190,8 @@ const UsersPage: NextPage = () => {
             <Card.Header>
               Create user
             </Card.Header>
-            <Card.Body>
-              <Form onSubmit={createUser}>
+            <Form onSubmit={createUser}>
+              <Card.Body>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                   <Form.Label>Email address</Form.Label>
                   <Form.Control required type="email" placeholder="example@email.com" />
@@ -136,20 +201,25 @@ const UsersPage: NextPage = () => {
                   <Form.Label>Full Name</Form.Label>
                   <Form.Control required type="name" placeholder="John Doe" />
                 </Form.Group>
+              </Card.Body>
+              <Card.Footer className='text-end'>
                 <Button variant="primary" type="submit">
                   Submit
                 </Button>
-              </Form>
-            </Card.Body>
+              </Card.Footer>
+            </Form>
           </Card>
-
           <br />
           <Card className="mb-4">
-            <Card.Header>
+            <Card.Header style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
               User Database
+              <Button variant="primary" style={{ justifySelf: 'flex-end' }} onClick={updateUsers}>
+                <FontAwesomeIcon icon={faSync} />
+              </Button>
+
             </Card.Header>
-            <Card.Body>
-              <div className="table-responsive">
+            <Card.Body >
+              <div style={{ height: '30vh' }} className="table-responsive">
                 <table className="table border mb-0">
                   <thead className="table-light fw-semibold">
                     <tr className="align-middle">
@@ -192,7 +262,9 @@ const UsersPage: NextPage = () => {
 
                             <Dropdown.Menu>
                               <Dropdown.Item href="#/action-1">Info</Dropdown.Item>
-                              <Dropdown.Item >Edit</Dropdown.Item>
+                              <Dropdown.Item
+                                onClick={() => { setModalVisible({ show: true, user_id: user._id }) }}
+                              >Edit</Dropdown.Item>
                               <Dropdown.Item
                                 className="text-danger"
                                 onClick={() => deleteUser(user._id)}
