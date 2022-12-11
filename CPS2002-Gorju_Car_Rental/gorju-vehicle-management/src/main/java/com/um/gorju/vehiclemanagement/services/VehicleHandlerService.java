52e5071c113eb635a85a1373dc724efd09c5e116
Julianne Vella <54096586+julnnev@users.noreply.github.com>
2022-12-11 13:57:00 +0100

    Amended application.properties, pom.xml files with required dependencies and Vehicle class with available field. Tests written

--- CPS2002-Gorju_Car_Rental/gorju-vehicle-management/src/main/java/com/um/gorju/vehiclemanagement/services/VehicleHandlerService.java
+++ CPS2002-Gorju_Car_Rental/gorju-vehicle-management/src/main/java/com/um/gorju/vehiclemanagement/services/VehicleHandlerService.java
@@ -1,6 +1,8 @@
 package com.um.gorju.vehiclemanagement.services;
 
+import com.um.gorju.vehiclemanagement.data.entities.VehicleEntity;
 import com.um.gorju.vehiclemanagement.data.repositories.VehicleRepository;
+import org.hibernate.type.descriptor.sql.JdbcTypeFamilyInformation;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,18 +15,60 @@ public class VehicleHandlerService {
     @Autowired
     VehicleRepository repository;
 
-    // public boolean addVehicle(VehicleSubmission vehicleSubmission) {
-        //add motorcycle, family car, commercial vehicle
+    public boolean addFamilyCar(FamilyCar familyCar) {
+        VehicleEntity vehicleEntity = mapper.map(familyCar, VehicleEntity.class);
+        boolean exists = repository.existsById(vehicleEntity.getNumberPlate());
 
-   //  public boolean deleteVehicle(VehicleSubmission vehicleSubmission) {
-        //delete motorcycle, family car, commercial vehicle
+        if (exists) {
+            return false;
+        } else {
+            repository.save(vehicleEntity);
+            return true;
+        }
+    }
+
+    public boolean addMotorcycle(Motorcycle motorcycle) {
+        VehicleEntity vehicleEntity = mapper.map(motorcycle, VehicleEntity.class);
+        boolean exists = repository.existsById(vehicleEntity.getNumberPlate());
+
+        if(exists){
+            return false;
+        }else{
+            repository.save(vehicleEntity);
+            return true;
+        }
+
+    }
+
+    public boolean addCommercialVehicle(CommercialVehicle commercialVehicle) {
+        VehicleEntity vehicleEntity = mapper.map(commercialVehicle, VehicleEntity.class);
+        boolean exists = repository.existsById(vehicleEntity.getNumberPlate());
+
+        if(exists){
+            return false;
+        }else{
+            repository.save(vehicleEntity);
+            return true;
+        }
+    }
+
+
+   public boolean deleteVehicle(String numberPlate) {
+       if(!repository.existsById(numberPlate))
+           return false;
+       repository.deleteById(numberPlate);
+       return true;
+   }
+
+
+   }
 
     // public Vehicle getVehiclebyNumberPlate(String numberPlate) {
 
     // public List<Vehicle> getVehicleByColour(String colour, RequestType requestType){
 
+    // public List<Vehicle> getAvailableCars(){
 
     // public boolean updateVehicle(UpdateVehicleRequest request){
 
 
-}
--- CPS2002-Gorju_Car_Rental/gorju-vehicle-management/src/test/java/com/um/gorju/vehiclemanagement/services/VehicleManagementServiceTests.java
+++ CPS2002-Gorju_Car_Rental/gorju-vehicle-management/src/test/java/com/um/gorju/vehiclemanagement/services/VehicleManagementServiceTests.java
@@ -1,4 +1,109 @@
 package com.um.gorju.vehiclemanagement.services;
 
+import com.um.gorju.vehiclemanagement.data.entities.VehicleEntity;
+import com.um.gorju.vehiclemanagement.data.repositories.VehicleRepository;
+import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
 public class VehicleManagementServiceTests {
+    @Autowired
+    VehicleHandlerService vehicleHandlerService;
+
+    @MockBean
+    VehicleRepository repository;
+
+    @Autowired
+    ModelMapper modelMapper;
+
+    @Test
+    public void testAddFamilyVehicle(){
+        // Setup
+        FamilyCar familyCar = new FamilyCar("ABC123", "Captur", "Renault", "White");
+        boolean expectedEntered = true;
+
+        when(repository.existsById(any(String.class))).thenReturn(false);
+
+        // Exercise
+        boolean entered = vehicleHandlerService.addFamilyCar(familyCar);
+
+        // Verify
+        assertEquals(expectedEntered, entered);
+
+        //Checking that .existsById is called once
+        verify(repository, times(1)).existsById(any(String.class));
+        //Checking that .save is called once
+        verify(repository, times(1)).save(any(VehicleEntity.class));
+        // Teardown -- no teardown needed
+    }
+
+    @Test
+    public void testAddMotorcycleVehicle(){
+        Motorcycle motorcycle = new Motorcycle("ABC234", "Tenere 700", "Yamaha", "Black");
+        boolean expectedEntered = true;
+
+        when(repository.existsById(any(String.class))).thenReturn(false);
+
+        // Exercise
+        boolean entered = vehicleHandlerService.addMotorcycle(motorcycle);
+
+        // Verify
+        assertEquals(expectedEntered, entered);
+
+        //Checking that .existsById is called once
+        verify(repository, times(1)).existsById(any(String.class));
+        //Checking that .save is called once
+        verify(repository, times(1)).save(any(VehicleEntity.class));
+        // Teardown -- no teardown needed
+    }
+
+    @Test
+    public void testAddCommercialVehicle(){
+        // Setup
+        CommercialVehicle commercialVehicle = new CommercialVehicle("ABF234", "Expert", "Peugeot", "Blue");
+        boolean expectedEntered = true;
+        when(repository.existsById(any(String.class))).thenReturn(false);
+
+        // Exercise
+        boolean entered = vehicleHandlerService.addCommercialVehicle(commercialVehicle);
+
+        // Verify
+        assertEquals(expectedEntered, entered);
+
+        //Checking that .existsById is called once
+        verify(repository, times(1)).existsById(any(String.class));
+        //Checking that .save is called once
+        verify(repository, times(1)).save(any(VehicleEntity.class));
+        // Teardown -- no teardown needed
+    }
+
+    @Test
+    public void deleteExistingVehicletest(){
+        String numberPlate = "ABC123";
+        //existsById returns true meaning vehicle exists in repository
+        when(repository.existsById(any(String.class))).thenReturn(true);
+        boolean expectedFound = true;
+
+        // Exercise
+        boolean found = vehicleHandlerService.deleteVehicle(numberPlate);
+
+        // Verify
+        assertEquals(expectedFound, found);
+
+        //Checking that .existsById is called once
+        verify(repository, times(1)).existsById(any(String.class));
+        //Checking that .delete is called once
+        verify(repository, times(1)).deleteById(any(String.class));
+
+        // Teardown -- no teardown needed
+
+    }
 }
+
+
