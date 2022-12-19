943c600ea35b18217cfc2a4134b1f69d416b729a
Julianne Vella <54096586+julnnev@users.noreply.github.com>
2022-12-19 11:53:08 +0100

    Update Vehicle Response Implementation

--- gorju/vehicle-management/src/main/java/com/um/gorju/vehiclemanagement/web/controllers/responses/UpdateVehicleResponse.java
+++ gorju/vehicle-management/src/main/java/com/um/gorju/vehiclemanagement/web/controllers/responses/UpdateVehicleResponse.java
@@ -1,4 +1,21 @@
 package com.um.gorju.vehiclemanagement.web.controllers.responses;
 
 public class UpdateVehicleResponse {
+    private boolean success;
+
+    public UpdateVehicleResponse(boolean success){
+        this.success = success;
+    }
+
+   public UpdateVehicleResponse(){
+
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
 }
