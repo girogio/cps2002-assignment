999ad19b8ff1bf7a578131d0011c2f0779e37380
Giorgio Grigolo <giorgiogrigolo03@gmail.com>
2022-12-31 18:23:49 +0100

    Fixed environment variable setting

--- gorju/booking-management/index.js
+++ gorju/booking-management/index.js
@@ -1,8 +1,8 @@
 
-express = require('express')
-mongoose = require('mongoose')
-cors = require('cors')
-dotenv = require('dotenv')
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'
 
 const app = express()
 const db = mongoose.connection
@@ -12,10 +12,21 @@ dotenv.config()
 app.use(express.json())
 app.use(cors())
 
+mongoose.set('strictQuery', true)
+
+if (process.env.NODE_ENV === 'development') {
+    process.env.USER_API_URL = 'http://localhost:3001/api/users/'
+    process.env.VEHICLE_API_URL = 'http://localhost:3002/api/vehicles/'
+} else {
+    process.env.USER_API_URL = 'http://user_app:3000/api/users/'
+    process.env.VEHICLE_API_URL = 'http://vehicleapp:3002/api/vehicles/'
+}
+
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 db.on('error', (error) => console.error(error))
 
-app.use('/api/booking', require('./routes/bookings.js'))
+import bookingRouter from './src/routes/bookings.js'
+app.use('/api/bookings/', bookingRouter)
 
 app.listen(process.env.SERVICE_PORT, () => console.log('Server started on port ' + process.env.SERVICE_PORT))
-db.once('open', () => console.log('Connected to database.' ))
+db.once('open', () => console.log('Connected to database.'))
